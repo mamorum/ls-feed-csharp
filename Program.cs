d571e0be5b822d5e7695c307993e93799f4d7669
mamorum <mamoru310@yahoo.com>
2018-05-15 16:10:59 +0900

    Import.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace ls_feed {
+  class Program {
+    static void Main(string[] args) {
+      Server.Start();
+      //-> open browser
+      string exeDir = Directory.GetParent(
+        Assembly.GetExecutingAssembly().Location
+      ).FullName.Replace('\\', '/');
+      System.Diagnostics.Process.Start(
+        "file:///" + exeDir + "/public/index.html"
+      );
+    }
+  }
+}
