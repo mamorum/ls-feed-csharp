49c208240b5fdc442d635d78bdfd088f160fdfe2
mamorum <mamoru310@yahoo.com>
2018-06-06 22:49:08 +0900

    Delete name.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("lsFeed")]
-[assembly: AssemblyCopyright("mamorum")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
