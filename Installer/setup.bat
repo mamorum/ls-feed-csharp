308da688d3518fc782ea1a71e00637b7b987999d
ma- <mamorum@users.noreply.github.com>
2019-11-12 10:41:36 +0900

    追加：インストーラー用の資源

---
+++ Installer/setup.bat
@@ -0,0 +1,13 @@
+@echo off
+echo.>.\unblock.ps1:Zone.Identifier
+powershell -NoProfile -ExecutionPolicy Unrestricted .\unblock.ps1
+set DST=%USERPROFILE%\lsFeed
+if exist "%DST%" (
+  rmdir "%DST%\Content" /S /Q
+  del "%DST%\lsFeed.exe" /Q
+  del "%DST%\version.txt" /Q
+) else (
+  mkdir "%DST%"
+)
+xcopy Build "%DST%" /S /E /F /G /H /R /K /Y /Q
+pause
---
+++ Installer/unblock.ps1
@@ -0,0 +1,1 @@
+ls -r . | Unblock-File
